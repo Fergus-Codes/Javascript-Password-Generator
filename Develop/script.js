45b80fddd76705d3ef32324820afcdbfdead60d1
Fergus Illman <fergus.illman.au@gmail.com>
2023-03-28 14:25:37 +1030

    Added array's for each character specification & an array for user input

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,12 @@ var generateBtn = document.querySelector("#generate");
 
 //TODO: Create array for each character specification
 
+var userinputArray = []; // An empty Array to store all user input
+
+var specialCharacter = ['!','@','#','$','%','^','&','*','(',')','_','-','=','+','[','{',']','}','|'];
+var lowercaseCharacter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var uppercaseCharacter = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','W','X','Y','Z'];
+var numberCharacters = ['1','2','3','4','5','6','7','8','9'];
 
 // Write password to the #password input
 function writePassword() {
